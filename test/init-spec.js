af9bbb06b11d0300aab884e20ba5c8de8988bb06
channing <cn_channing@hotmail.com>
2015-02-04 00:04:38 +0800

    test

--- test/init-spec.js
+++ test/init-spec.js
@@ -1,26 +1,29 @@
-var expect =require("chai").expect;
+var expect = require("chai").expect;
 var fs = require("q-io/fs");
-describe("cli init",function(){
-    beforeEach(function(){
-      return fs.removeTree("testDir")
-      .then(function(){
-        return fs.makeDirectory("testDir")
-      })
-      .then(function(){
+var exec = require('child_process').exec;
+var path = require('path');
+describe("cli init", function() {
+  beforeEach(function() {
+    if (fs.exists("testDir")) {
+       fs.removeTree("testDir");
+    }
+    return fs.makeDirectory("testDir")
+      .then(function() {
         return fs.makeDirectory("testDir/testDir1")
       })
-      .then(function(){
+      .then(function() {
         return fs.makeDirectory("testDir/testDir2")
       })
-      .then(function(){
-        return fs.write("testDir/testDir2/hello.txt","hello word!\n")
+      .then(function() {
+        return fs.write("testDir/testDir2/hello.txt", "hello word!\n")
       })
-    });
-    it("init ",function(){
-      expect(1).to.equal(1);
+  });
+  it("init without name", function(done) {
+    exec("cd testDir", function() {
     })
+  })
 
-    afterEach(function(){
-      return fs.removeTree("testDir");
-    })
+  afterEach(function() {
+    return fs.removeTree("testDir");
+  })
 })
